ed6f771eedeb0f730ada4b3501d00c2d75f63f12
Yashwant Bhosale <yashwantbhosale07@gmail.com>
2025-04-05 13:47:59 +0530

    fix: added env generation in init script

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ sudo apt install tmux
 sudo apt install nodejs
 sudo apt install npm
 sudo npm install -g nodemon
+
+cd server
+touch .env
+printf "MONGO_URI=mongodb://localhost:27017/course_management_test
+PORT=5000\n" >> .env
+cd ..
